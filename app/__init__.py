2a056c0b2ac0f9fd5b19585f4ed05977d3985379
ziguang <ziguangzhiye@gmail.com>
2016-12-23 13:49:33 +0800

    init commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 
 myapp = Flask(__name__)
 myapp.config.from_object('config')
@@ -15,4 +16,24 @@ loginmanager.init_app(myapp)
 loginmanager.login_view = 'login'
 oldid = OpenID(myapp, os.path.join(basedir, 'tmp'))
 
+if not myapp.debug:
+    #import logging
+    #from logging.handlers import SMTPHandler
+    #credentials = None
+    #if MAIL_USERNAME or MAIL_PASSWORD:
+    #    credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+    #mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS,
+    #                            'microblog failure', credentials)
+    #mail_handler.setLevel(logging.ERROR)
+    #myapp.logger.addHandler(mail_handler)
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(
+    logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    
+    myapp.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    myapp.logger.addHandler(file_handler)
+    myapp.logger.info('microblog startup')
 from app import views, models
